3fb7fd8843640a3af68eb246306b4dcf2543e1a3
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-12-25 07:15:58 +0530

    snippet for mobile Auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import MobileAuth from "./component/MobileAuth";
 import SimpleAuth from "./component/SimpleAuth";
 
 export default function App() {
   return (
     <div>
       <SimpleAuth />
+      <MobileAuth/>    
     </div>
   );
 }
---
+++ src/component/MobileAuth.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function MobileAuth(){
+    const countryCode ="+91";
+    const [phoneNumber, setPhoneNumbere] =useState(countryCode);
+    const [expandForm, setExpandForm] = useState(false);
+
+    const requestOTP = (e)=>{
+        e.preventDefault();
+        if(phoneNumber.length >= 12){
+            setExpandForm(true);
+        }
+    }
+    return(
+        <div className="container">
+            <from onSubmit={requestOTP}>
+                    <h1>Sign in with phone number</h1>
+                    <div className="mb-3">
+                        <label htmlFor="phoneNumberInput" className="form-label">Phone number</label>
+                        <input type="tel" className="form-control" id="phoneNumberInput" aria-describedby="phoneHelp" />
+                        <div id="phoneNumberHelp" className="form-text">Please enter your phone number</div>
+                    </div>
+                    {expandForm === true ?
+                    <>
+                    <div className="mb-3">
+                        <label htmlFor="otpInput" className="form-label">OTP</label>
+                        <input type="number" className="form-control" id="otpInput"/>
+                        <div id="otpHelp" className="form-text">Plese inter your phone number</div>
+                    </div>
+                    </>:null 
+                }
+                {expandForm === false ? <button className="btn btn-primary">Request OTP</button>:null}
+                <div id="recaptcha-container"></div>
+            </from>
+        </div>
+    );
+}
